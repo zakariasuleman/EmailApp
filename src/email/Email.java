3725f5da913387b9f8c12d91d3646037bee839d7
Zakaria Suleman <zakariasuleman@zs.local>
2019-05-12 00:52:42 -0500

    Added method that asks for department and sets it

--- src/email/Email.java
+++ src/email/Email.java
@@ -1,5 +1,7 @@
 package email;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -13,9 +15,23 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " "+ this.lastName);
+
+        // Call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department" + " "+this.department);
     }
 
     // Ask for the department
+    private String setDepartment(){
+        System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting \n0 for none\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+
+        if(depChoice ==1){return "sales"; }
+        else if(depChoice ==2){return "dev";}
+        else if(depChoice ==3){return "accounting";}
+        else{ return ""; }
+    }
 
     // Generate a random password
 
